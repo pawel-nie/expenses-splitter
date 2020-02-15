882d2728e484edee69dd6ac55fda659fbd4f0bde
pawel-nie <pawelniedbala93@gmail.com>
2020-02-15 21:08:16 +0100

    Initial commit

---
+++ src/main/java/com/pawelnie/expensessplitter/ExpensesSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.pawelnie.expensessplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesSplitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawelnie/expensessplitter/ExpensesSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelnie.expensessplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
